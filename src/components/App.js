793d82da5a3e5adfea7f6db39311947c2b5c619c
Gracelaura <gracenjuguna05@gmail.com>
2022-09-27 00:30:15 +0300

    Added changes

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,18 @@ import React from "react";
 import MovieCard from "./MovieCard";
 
 function App() {
+  const socialLinks = {
+    github: "https://github.com/liza",
+    linkedin: "https://www.linkedin.com/in/liza/",
+  };
   const title = "Mad Max";
-  const posterURL =
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn1OTYGz2GDC1XjA9tirh_1Rd571yE5UFIYsmZp4nACMd7CCHM";
+  const posterURL ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn1OTYGz2GDC1XjA9tirh_1Rd571yE5UFIYsmZp4nACMd7CCHM";
   const genresArr = ["Action", "Adventure", "Science Fiction", "Thriller"];
 
   return (
     <div className="App">
       <MovieCard title={title} genres={genresArr} />
+      <SocialMedia links={socialLinks} />
     </div>
   );
 }
